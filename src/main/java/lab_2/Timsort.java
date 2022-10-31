f208002bf4c8c50cefe2d4b3115c38a192a430e3
mayosen <gare577@gmail.com>
2022-10-31 21:10:03 +0300

    Separate list to ordered sublists

--- src/main/java/lab_2/Timsort.java
+++ src/main/java/lab_2/Timsort.java
@@ -1,21 +1,56 @@
 package lab_2;
 
+import lab_1.ArrayList;
 import lab_1.List;
 
 import java.util.Comparator;
+import java.util.logging.Logger;
 
 public class Timsort {
-    public static int getMinrun(int n) {
-        int r = 0;
+    private static final Logger logger = Logger.getLogger(Timsort.class.getName());
+
+    public static int getMinRun(int n) {
+        int flag = 0;
         while (n >= 64) {
-            r |= (n & 1);
+            flag |= (n & 1);
             n >>= 1;
         }
-
-        return n + r;
+        return n + flag;
     }
 
-    public static <T> void timsort(List<T> list, Comparator<T> comparator) {
+    public static <T> void timsort(ArrayList<T> list, Comparator<T> cmp) {
+        int minRun = getMinRun(list.size());
+        int start = 0;
+        List<List<T>> subLists = new ArrayList<>();
 
+        while (start < list.size() - 1) {
+            int current = start;
+
+            while (current + 1 < list.size() && cmp.compare(list.get(current), list.get(current + 1)) <= 0) {
+                current++;
+            }
+
+            int increasingRun = current + 1;
+
+            current = start;
+
+            while (current + 1 < list.size() && cmp.compare(list.get(current), list.get(current + 1)) > 0) {
+                current++;
+            }
+
+            int decreasingRun = current + 1;
+
+            int maxRun = Math.max(increasingRun, decreasingRun);
+            ArrayList<T> subList = list.subList(start, maxRun);
+            subLists.add(subList);
+
+            if (decreasingRun > increasingRun) {
+                subList.reverse();
+            }
+
+            start = maxRun;
+        }
+
+        System.out.println(4);
     }
 }
--- src/test/java/lab_2/TimsortTest.java
+++ src/test/java/lab_2/TimsortTest.java
@@ -1,13 +1,15 @@
 package lab_2;
 
+import lab_1.ArrayList;
 import org.junit.jupiter.api.Test;
 
-import static lab_2.Timsort.getMinrun;
+import static lab_2.Timsort.getMinRun;
+import static lab_2.Timsort.timsort;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TimsortTest {
     private void printMinrun(int value) {
-        System.out.format("minrun for %d = %d\n", value, getMinrun(value));
+        System.out.format("minrun for %d = %d\n", value, getMinRun(value));
     }
 
     @Test
@@ -20,10 +22,20 @@ class TimsortTest {
         printMinrun(45);
         printMinrun(64);
         printMinrun(100);
+        printMinrun(269);
+        printMinrun(356);
         printMinrun(1_000);
         printMinrun(1_000_000);
         printMinrun(10_000_000);
         printMinrun(100_000_000);
         printMinrun(1_000_000_000);
+    }
+
+    @Test
+    void runs() {
+        ArrayList<Integer> list = new ArrayList<>(new Integer[]{
+                1, 2, 3, 25, 23, 35, 4, 5, 7, 9, 13, 6, 8, 7, 6, 5, 4, 3, 2, 1
+        });
+        timsort(list, Integer::compare);
     }
 }
