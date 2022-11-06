3689bb357ad8e705b7a85a63322c47215505cf72
mayosen <gare577@gmail.com>
2022-11-06 14:05:26 +0300

    Check invariant as required

--- src/main/java/lab_2/TimSort.java
+++ src/main/java/lab_2/TimSort.java
@@ -15,7 +15,7 @@ public class TimSort {
         System.out.format(format + "\n", args);
     }
 
-    public static <T> String elementsString(List<T> list, int from, int to) {
+    private static <T> String elementsString(List<T> list, int from, int to) {
         StringBuilder builder = new StringBuilder();
         builder.append("[");
         for (int i = from; i < to; i++) {
@@ -28,21 +28,19 @@ public class TimSort {
         return builder.toString();
     }
 
-    public static <T> String elementsString(List<T> list, SubList subList) {
+    private static <T> String elementsString(List<T> list, SubList subList) {
         return elementsString(list, subList.start, subList.end);
     }
 
-    public static <T> String elementsString(List<T> list) {
+    private static <T> String elementsString(List<T> list) {
         return elementsString(list, 0, list.size());
     }
 
     public static <T> void timSort(List<T> list, Comparator<T> cmp) {
-        // log("sorting: %s", elementsString(list));
+        Stack<SubList> stack = new ArrayList<>();
         int minRun = getMinRun(list.size());
         // log("minRun: %d", minRun);
-
         int start = 0;
-        Stack<SubList> stack = new ArrayList<>();
 
         while (start < list.size()) {
             int current = start;
@@ -60,22 +58,20 @@ public class TimSort {
 
             int decreasingRun = current + 1;
             int maxRun = Math.max(increasingRun, decreasingRun);
+            // log("found run with length: %d", maxRun - start);
 
             if (decreasingRun > increasingRun) {
                 reverse(list, start, maxRun);
-                // log("reversed: %s", elementsString(list, start, maxRun));
             }
 
             if (maxRun - start < minRun) {
-                maxRun = Math.min(maxRun + (minRun - (maxRun - start)), list.size());
-                // log("extending run to minRun: %s", elementsString(list, start, maxRun));
+                maxRun = Math.min(start + minRun, list.size());
+                // log("run extended to: %d", maxRun);
             }
 
             SubList subList = new SubList(start, maxRun);
-            // log("new %s: %s", subList, elementsString(list, subList));
-
+            // log("new %s", subList);
             insertionSort(list, subList.start, subList.end, cmp);
-            // log("sorted: %s", elementsString(list, subList));
 
             stack.push(subList);
             mergeCollapse(list, stack, cmp);
@@ -86,7 +82,7 @@ public class TimSort {
         mergeForceCollapse(list, stack, cmp);
     }
 
-    public static int getMinRun(int n) {
+    static int getMinRun(int n) {
         int flag = 0;
         while (n >= 64) {
             flag |= (n & 1);
@@ -95,6 +91,9 @@ public class TimSort {
         return n + flag;
     }
 
+    /**
+     * Класс, описывающий границы подмассива.
+     */
     static class SubList {
         private final int start;
         private final int end;
@@ -120,7 +119,14 @@ public class TimSort {
         }
     }
 
-    public static <T> void reverse(List<T> list, int from, int to) {
+    /**
+     * Разворот элементов подмассива.
+     *
+     * @param list список
+     * @param from индекс, начиная с которого разворачивать (включительно)
+     * @param to индекс, до которого разворачивать (не включительно)
+     */
+    static <T> void reverse(List<T> list, int from, int to) {
         to--;
         while (from < to) {
             T temp = list.get(from);
@@ -134,39 +140,49 @@ public class TimSort {
     /**
      * Слияние двух подмассивов, если они не выполняют инвариант.
      */
-    public static <T> void mergeCollapse(List<T> list, Stack<SubList> stack, Comparator<T> cmp) {
-        while (stack.size() >= 3) {
+    static <T> void mergeCollapse(List<T> list, Stack<SubList> stack, Comparator<T> cmp) {
+        while (stack.size() >= 2) {
             SubList listX = stack.pop();
             SubList listY = stack.pop();
-            SubList listZ = stack.pop();
 
-            if (!(listY.length > listX.length)) {
-                if (listX.length < listZ.length) {
+            if (stack.isEmpty()) {
+                if (!(listY.length > listX.length)) {
                     // log("merging %s and %s", listY, listX);
                     merge(list, listY, listX, cmp);
-                    stack.push(listZ);
                     stack.push(new SubList(listY.start, listX.end));
                 } else {
-                    // log("merging %s and %s", listZ, listY);
-                    merge(list, listZ, listY, cmp);
-                    stack.push(new SubList(listZ.start, listY.end));
+                    // log("invariant is observed")
+                    stack.push(listY);
                     stack.push(listX);
                 }
-            } else if (!(listZ.length > listX.length + listY.length)) {
-                // log("merging %s and %s", listZ, listY);
-                merge(list, listZ, listY, cmp);
-                stack.push(new SubList(listZ.start, listY.end));
-                stack.push(listX);
-            } else {
-                stack.push(listZ);
-                stack.push(listY);
-                stack.push(listX);
                 break;
+            } else {
+                SubList listZ = stack.pop();
+
+                if (!((listZ.length > listX.length + listY.length) && (listY.length > listX.length))) {
+                    if (listX.length < listZ.length) {
+                        // log("merging %s and %s", listY, listX);
+                        merge(list, listY, listX, cmp);
+                        stack.push(listZ);
+                        stack.push(new SubList(listY.start, listX.end));
+                    } else {
+                        // log("merging %s and %s", listZ, listY);
+                        merge(list, listZ, listY, cmp);
+                        stack.push(new SubList(listZ.start, listY.end));
+                        stack.push(listX);
+                    }
+                } else {
+                    // log("invariant is observed")
+                    stack.push(listZ);
+                    stack.push(listY);
+                    stack.push(listX);
+                    break;
+                }
             }
         }
     }
 
-    public static <T> void mergeForceCollapse(List<T> list, Stack<SubList> stack, Comparator<T> cmp) {
+    static <T> void mergeForceCollapse(List<T> list, Stack<SubList> stack, Comparator<T> cmp) {
         while (stack.size() >= 2) {
             SubList listX = stack.pop();
             SubList listY = stack.pop();
@@ -179,12 +195,8 @@ public class TimSort {
     /**
      * Слияние двух соседних подмассивов.
      */
-    public static <T> void merge(List<T> list, SubList left, SubList right, Comparator<T> cmp) {
-        List<T> leftCopy = new ArrayList<>(left.length);
-        for (int i = left.start; i < left.end; i++) {
-            leftCopy.add(list.get(i));
-        }
-
+    static <T> void merge(List<T> list, SubList left, SubList right, Comparator<T> cmp) {
+        List<T> leftCopy = list.subList(left.start, left.end);
         int l = left.start;
         int r = right.start;
         int current = left.start;
@@ -208,7 +220,7 @@ public class TimSort {
 
                 if (leftSeries == GALLOP_LENGTH) {
                     int end = findSeriesEnd(leftCopy, leftIndex - 1, left.length, list.get(r), cmp);
-                    // log("left gallop from %d to %d", l - 1, end);
+                    // log("left gallop from %d to %d", l - 1, left.start + end);
                     for (; l <= end; l++, current++) {
                         leftIndex = l - left.start;
                         list.set(current, leftCopy.get(leftIndex));
@@ -261,7 +273,7 @@ public class TimSort {
      * @param cmp компаратор
      * @return индекс последнего подходящего элемента
      */
-    public static <T> int findSeriesEnd(List<T> list, int from, int to, T limitValue, Comparator<T> cmp) {
+    static <T> int findSeriesEnd(List<T> list, int from, int to, T limitValue, Comparator<T> cmp) {
         int left = from;
         int right = to - 1;
 
